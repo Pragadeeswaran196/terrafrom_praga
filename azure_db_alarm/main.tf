48723ab7a9c9bfdd5615ee55504ad973ff783a69
Pragadeeswaran196 <101637224+Pragadeeswaran196@users.noreply.github.com>
2023-06-17 19:31:38 +0530

    Update main.tf

--- azure_db_alarm/main.tf
+++ azure_db_alarm/main.tf
@@ -100,7 +100,7 @@ resource "azurerm_monitor_metric_alert" "storage_alarm" {
   }
 }
 
-resource "azurerm_monitor_metric_alert" "io_consumption_percent_alarm" {
+resource "azurerm_monitor_metric_alert" "iops_alarm" {
     count = local.io_consumption_percent_present ? length(var.db_name) : 0
   name                = "Critical | prod | Azure US | microsoft.dbforpostgresql-servers | ${element(var.db_name, count.index)} | ${var.env[1]} | Average io_consumption_percent GreaterThan Upper Threhold"
   resource_group_name = var.env[0]
@@ -111,10 +111,10 @@ resource "azurerm_monitor_metric_alert" "io_consumption_percent_alarm" {
   severity                 = 0
   criteria {
     metric_namespace = "${local.check}"
-    metric_name      = "io_consumption_percent"
+    metric_name      = "iops"
     aggregation      = "Average"
     operator         = "GreaterThan"
-    threshold        = var.metrics["io_consumption_percent"]
+    threshold        = var.metrics["iops"]
   }
   action {
     action_group_id = "/subscriptions/87a888f1-2e99-44d3-8a58-dc56744d321d/resourceGroups/prod/providers/Microsoft.Insights/actionGroups/${var.notification}"
@@ -158,34 +158,6 @@ resource "azurerm_monitor_metric_alert" "active_connections_alarm" {
   }
 }
 
-resource "azurerm_monitor_metric_alert" "serverlog_storage_alarm" {
-    count = local.serverlog_storage_precent_present ? length(var.db_name) : 0
-  name                = "Critical | prod | Azure US | microsoft.dbforpostgresql-servers | ${element(var.db_name, count.index)} | ${var.env[1]} | Average serverlog_storage_percent GreaterThan Upper Threhold"
-  resource_group_name = var.env[0]
-  scopes              = ["/subscriptions/87a888f1-2e99-44d3-8a58-dc56744d321d/resourcegroups/prod/providers/${local.check}/${element(var.db_name, count.index)}"]
-  description         = "Action will be triggered when serverlog_storage_alarm is greater"
-  frequency                = "PT1M"
-  window_size              = "PT5M"
-  severity                 = 0
-  criteria {
-    metric_namespace = "${local.check}"
-    metric_name      = "serverlog_storage_percent"
-    aggregation      = "Average"
-    operator         = "GreaterThan"
-    threshold        = var.metrics["serverlog_storage_percent"]
-  }
-  action {
-    action_group_id = "/subscriptions/87a888f1-2e99-44d3-8a58-dc56744d321d/resourceGroups/prod/providers/Microsoft.Insights/actionGroups/${var.notification}"
-    }
-  action{
-    action_group_id = "/subscriptions/87A888F1-2E99-44D3-8A58-DC56744D321D/resourceGroups/fourkites-prod/providers/microsoft.insights/actionGroups/notify-dba"
-  }
-  tags = {
-    terraform_managed = "True"
-    env = var.env[0]
-    team = var.env[1]
-  }
-}
 
 resource "azurerm_monitor_metric_alert" "connections_failed_alarm" {
     count = local.connections_failed_present ? length(var.db_name) : 0
